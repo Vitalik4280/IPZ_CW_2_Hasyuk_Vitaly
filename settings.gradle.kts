1aa73d8a8ebc5c81aae91180fe13036bbe4d78c1
xm1ls <xm1ls2001@gmail.com>
2024-03-11 22:31:05 +0200

    new project

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "IPZ_CW_2_Hasyuk_Vitaly"
+include(":app")
