59aa552e6967b5770784ff7aab442567c1092bff
xm1ls <xm1ls2001@gmail.com>
2024-03-12 14:06:06 +0200

    Add state values

--- app/src/main/java/ua/edu/lntu/cw_2/MainActivity.kt
+++ app/src/main/java/ua/edu/lntu/cw_2/MainActivity.kt
@@ -8,7 +8,12 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import ua.edu.lntu.cw_2.ui.theme.IPZ_CW_2_Hasyuk_VitalyTheme
 
@@ -27,6 +32,13 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+}
+
+@Composable
+fun SignInPage(modifier: Modifier = Modifier) {
+    var email by remember { mutableStateOf(TextFieldValue()) }
+    var password by remember { mutableStateOf(TextFieldValue()) }
+    var signInSuccess by remember { mutableStateOf(false) }
 }
 
 @Preview(showBackground = true)
